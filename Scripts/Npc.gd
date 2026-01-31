d74aba9666408478f1f54236cdd9198eff218f80
YisusFls <jesusfernando063@gmail.com>
2026-01-31 18:23:18 -0300

    Randomizado de Cabeza y Cuerpo

--- Scripts/Npc.gd
+++ Scripts/Npc.gd
@@ -3,10 +3,13 @@ class_name Npc extends Node2D
 @export var interactable: Interactable
 @export var head:Sprite2D
 @export var body: Sprite2D
+@export var AllPosibilities:ResourceAllPossibilities
 
 func _ready():
 	interactable.signal_interactable.connect(on_interacted)
-	pass
+	head.texture = AllPosibilities.heads[randi_range(0,2)].image
+	body.texture = AllPosibilities.bodies[randi_range(0,2)].image
+	
 
 func on_interacted():
 	print("%s: on_interact" % [name])
