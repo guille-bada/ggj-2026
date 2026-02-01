bf51dea54f33144e56553b7b3b15197efe63ef63
Daniel K <danielkomes@gmail.com>
2026-02-01 12:16:41 -0300

    Add logic to prevent non-solution NPCs to use the solution combination.

--- Scripts/Globals/LogicSingleton.gd
+++ Scripts/Globals/LogicSingleton.gd
@@ -3,15 +3,26 @@ extends Node
 @export var solution: ResourceSolution
 @export var clues_found: Array[ResourceClue]
 @export var all_possibilities: ResourceAllPossibilities = preload("uid://ckfq6o77hhe5j")
+var non_solution_possibilities: ResourceAllPossibilities
 
 func _ready() -> void:
-	var head:ResourceClue = all_possibilities.heads[randi_range(0, all_possibilities.heads.size()-1)]
-	var body:ResourceClue = all_possibilities.bodies[randi_range(0, all_possibilities.bodies.size()-1)]
-	var location:ResourceClue = all_possibilities.locations[randi_range(0, all_possibilities.locations.size()-1)]
-	var scene:ResourceScene = all_possibilities.scenes[randi_range(0, all_possibilities.scenes.size()-1)]
+	var head: ResourceClue = all_possibilities.heads[randi_range(0, all_possibilities.heads.size() - 1)]
+	var body: ResourceClue = all_possibilities.bodies[randi_range(0, all_possibilities.bodies.size() - 1)]
+	var location: ResourceClue = all_possibilities.locations[randi_range(0, all_possibilities.locations.size() - 1)]
+	var scene: ResourceScene = all_possibilities.scenes[randi_range(0, all_possibilities.scenes.size() - 1)]
 	solution = ResourceSolution.new()
 	solution.head = head
 	solution.body = body
 	solution.location = location
 	solution.scene = scene
+
+	get_non_solution_possibilities()
 	pass
+
+func get_non_solution_possibilities() -> ResourceAllPossibilities:
+	non_solution_possibilities = all_possibilities.duplicate(true)
+	non_solution_possibilities.heads.erase(solution.head)
+	non_solution_possibilities.bodies.erase(solution.body)
+	non_solution_possibilities.locations.erase(solution.location)
+	non_solution_possibilities.scenes.erase(solution.scene)
+	return non_solution_possibilities
--- Scripts/Npc.gd
+++ Scripts/Npc.gd
@@ -3,14 +3,13 @@ class_name Npc extends Node2D
 @export var interactable: Interactable
 @export var head: Sprite2D
 @export var body: Sprite2D
-@export var AllPosibilities: ResourceAllPossibilities
 
 func _ready():
 	interactable.signal_interact_start.connect(on_interact_start)
 	interactable.signal_interact_end.connect(on_interact_end)
 	
-	head.texture = AllPosibilities.heads[randi_range(0, 2)].image
-	body.texture = AllPosibilities.bodies[randi_range(0, 2)].image
+	head.texture = LogicSingleton.non_solution_possibilities.heads[randi_range(0, LogicSingleton.non_solution_possibilities.heads.size() - 1)].image
+	body.texture = LogicSingleton.non_solution_possibilities.bodies[randi_range(0, LogicSingleton.non_solution_possibilities.bodies.size() - 1)].image
 	
 
 func on_interact_start():
