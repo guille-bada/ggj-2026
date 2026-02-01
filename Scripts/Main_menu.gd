1a283dafdc99213a2c0c79c236d5ca746e593c7d
YisusFls <jesusfernando063@gmail.com>
2026-02-01 13:07:45 -0300

    creacion del main menu con fade

---
+++ Scripts/Main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@export  var fade_menu:  TransitionFade
+var did_press_any_button : bool = false
+
+func _ready() -> void:
+	fade_menu.fade_out()
+
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventKey :
+		
+		did_press_any_button = true
+		
+		get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+		
+		
+		
+		
