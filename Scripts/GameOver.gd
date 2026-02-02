11dc42e95947d59b920feb9a8df4db85781ad9ec
Daniel K <danielkomes@gmail.com>
2026-02-02 17:45:25 -0300

    Add start and end sounds. Fix some scenes not being centered.

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,7 +1,10 @@
 class_name GameOver extends Control
 
+@export var audio_player: AudioStreamPlayer
+
 func interact():
 	await create_tween().tween_property(self , "modulate:a", 1, 1).finished
+	audio_player.play()
 	await get_tree().create_timer(3).timeout
 	LogicSingleton._ready()
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
